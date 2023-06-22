2e7c14edcf73cf7b4a9134ca3598ecab29a166d9
Dmitri Morozov <dmitri.m80@gmail.com>
2023-06-22 11:25:40 -0400

    Initial Commit

---
+++ Swift Lessons/Swift_LessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_LessonsApp.swift
+//  Swift Lessons
+//
+//  Created by Dmitri Morozov on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_LessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
